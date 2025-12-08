c61730f9efaa5200ae8cda4e99d6668892108be8
fhpeerless <126315532+fhpeerless@users.noreply.github.com>
2025-12-08 11:33:40 +0800

    Update yingyong.js

--- js/yingyong.js
+++ js/yingyong.js
@@ -20,8 +20,8 @@ const appData = [
             <h4>更新日志</h4>
             <p>v2.1.0 (2025-01-15)：新增AI总结功能，支持一键生成笔记摘要</p>
         `,
-        imgUrl: "https://i0.hdslb.com/bfs/mallup/mall/2x/03/2x03zy10x0zy02x23xx302zzw3x300w0.png@.webp",
-        videoEmbed: "<iframe src=\"https://player.bilibili.com/player.html?isOutside=true&aid=1303107810&bvid=BV1LM4m1X7dk&cid=1508435093&p=1\" scrolling=\"no\" border=\"0\" frameborder=\"no\" framespacing=\"0\" allowfullscreen=\"true\"></iframe>",
+        imgUrl: "https://cdn.pixabay.com/photo/2024/10/18/20/50/candle-9131407_960_720.jpg",
+        videoEmbed: "",
         downloadLinks: [
             { platform: "源码购买", link: "https://mall.bilibili.com/neul-next/detailuniversal/detail.html?isMerchant=1&page=detailuniversal_detail&saleType=10&itemsId=12328456&loadingShow=1&noTitleBar=1&msource=merchant_share" },
             { platform: "源码购买", link: "https://mall.bilibili.com/neul-next/detailuniversal/detail.html?isMerchant=1&page=detailuniversal_detail&saleType=10&itemsId=12328456&loadingShow=1&noTitleBar=1&msource=merchant_share" }
